66b5ce41654d296d587876fc3baed8e371a59b71
entsarahmed <entesara500@gmail.com>
2024-09-07 19:02:22 +0300

    Action ReturnTypes

--- Project Structure/Controllers/MoviesController.cs
+++ Project Structure/Controllers/MoviesController.cs
@@ -4,6 +4,11 @@ namespace Project_Structure.Controllers
 {
     public class MoviesController:Controller
     {
+        private readonly IConfiguration _configuration;
+        public MoviesController(IConfiguration configuration) // ASK CLR for Creating Object from Class Implementing the Interface "IConfiguration
+        {
+            _configuration = configuration;
+        }
 
         //Action: Public Non-Static Object Member Method inside the Controller
         [ActionName("Index")]
@@ -49,10 +54,73 @@ namespace Project_Structure.Controllers
         ////}
         ///
 
-        public string GetMovie(int id)
-        {
-            return $"Movie With Id: {id}";
-        }
+        //public string GetMovie(int id)
+        //{
+        //    return $"Movie With Id: {id}";
+        //}
+
+
+
+
+        #region Action ReturnTypes
+        //public IActionResult GetMovie(int id) 
+        //{
+        //    if (id == 0)
+        //        // return new BadRequestResult();
+        //        return BadRequest();
+
+
+        //    if (id == 100)
+        //        //return new NotFoundResult();
+        //        return NotFound();
+
+
+        //   //ContentResult result = new ContentResult();
+        //   // result.Content = $"<h1>Movie with Id: {id}<h1>";
+        //   //// result.ContentType="object/pdf";
+
+        //   //     result.ContentType = "text/html";
+        //   //     // result.StatusCode=200;
+
+        //   // return result;
+
+
+        //    return Content($"<h1>Movie with Id: {id}<h1>", "text/html");
+
+
+
+        //}
+
+        //[HttpGet] // Get: baseUrl/Movies/Hamada
+        //public IActionResult Hamada()
+        //{
+        //    // RedirectResult result = new RedirectResult("https://wuzzuf.net/jobs/egypt");
+        //    // RedirectResult result = new RedirectResult(_configuration["WuzzufUrl"] ?? string.Empty);
+
+        //    //RedirectResult result = new RedirectResult("https://localhost:5095/Movies/GetMovie/10");
+        //    //RedirectResult result = new RedirectResult($"{_configuration["BaseUrl"]}Movies/GetMovie/10");
+        //    // return result;
+
+
+
+        //    // return Redirect("https://wuzzuf.net/jobs/egypt");
+        //    // return Redirect(_configuration["WuzzufUrl"] ?? string.Empty);
+
+        //    //return RedirectToAction(nameof(GetMovie), "Movies", new { id = 10 });
+
+
+        //    //RedirectToActionResult result = new RedirectToActionResult(nameof(GetMovie), "Movies",new {id=10});
+        //    // return result;
+
+        //    //RedirectToRouteResult result = new RedirectToRouteResult("default",null);
+
+        //    //return result;
+        //    //return RedirectToRoute("default",null);
+
+        //} 
+        #endregion
+
+
 
     }
 }
