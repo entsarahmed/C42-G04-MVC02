eb84f5dba94a9e85faa9109065fe0f927b85d5ef
entsarahmed <entesara500@gmail.com>
2024-09-07 16:15:42 +0300

    Important Revision

--- Project Structure/Program.cs
+++ Project Structure/Program.cs
@@ -4,11 +4,40 @@ namespace Project_Structure
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            var webApplicationBuilder = WebApplication.CreateBuilder();
+
+
+
+            #region Configure Services
+
+            webApplicationBuilder.Services.AddControllersWithViews();
+
+            #endregion
+
+            var app = webApplicationBuilder.Build();
+
+            #region Configure
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage(); //Middle Ware
+            }
+            else
+            {
+                app.UseStatusCodePagesWithReExecute("/Home/Error");
+            }
+            app.UseRouting();
+
 
             app.MapGet("/", () => "Hello World!");
 
+
+
+            app.MapGet("/Hamada", async (context) =>
+            {
+                await context.Response.WriteAsync("Hello Hamada!");
+            });
+
+            #endregion
             app.Run();
         }
     }
