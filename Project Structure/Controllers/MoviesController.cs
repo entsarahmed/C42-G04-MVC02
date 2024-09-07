a20667629ce656c1133f5dba7c8b1662d65e3074
entsarahmed <entesara500@gmail.com>
2024-09-07 16:29:29 +0300

    Routing

---
+++ Project Structure/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Structure.Controllers
+{
+    public class MoviesController:Controller
+    {
+
+        //Action: Public Non-Static Object Member Method inside the Controller
+        [ActionName("Index")]
+        [HttpGet] // Get: baseUrl/Movies/GetMovie/{id}?name=avatar
+        public string GetAllMovie()
+        {
+            return "All Movies";
+        }
+
+
+
+
+
+
+        //public string GetMovie (int code)
+        //{
+        //    return $"Movie With Code: {code}";//0
+        //}
+
+        //  UrlPath//Movies/GetMovie/10?name=Avatar
+
+
+
+        //  [Authorize(Roles ="VIP")]
+        //public string GetMovie(int id, string name)
+        //{
+        //  return $"Movie with Name {name} and Movie With Id: {id}";
+        //}
+
+
+        ////[AcceptVerbs("Get","Post")]
+        ////[HttpGet]
+        ////public ViewResult CreateMovie()
+        ////{
+        ////    return new ViewResult();
+        ////}
+
+        ////[ActionName("ConfirmCreateMovie")]
+        ////[HttpPost]
+        ////public OkResult CreateMovie( Movie Model)
+        ////{
+        ////    return new OkResult();
+        ////}
+        ///
+
+        public string GetMovie(int id)
+        {
+            return $"Movie With Id: {id}";
+        }
+
+    }
+}
--- Project Structure/Program.cs
+++ Project Structure/Program.cs
@@ -36,6 +36,14 @@ namespace Project_Structure
             {
                 await context.Response.WriteAsync("Hello Hamada!");
             });
+            app.MapControllerRoute(
+                  name: "default",
+                  // pattern /*UrlPath*/:"Hamada/{controller}/{action}/{id?}"// static Segment
+                  //pattern /*UrlPath*/: "Hamada/XXX{controller}/{action}/{id?}"// Mixed Segment
+                  // pattern /* UrlPath*/: "{controller =Movies}/{action = GetMovie}/{id?}"// constructor + action + default
+                  pattern /* UrlPath*/: "{controller}/{action}/{id:int?}"//  constructor + action + default + constraints
+
+              );
 
             #endregion
             app.Run();
