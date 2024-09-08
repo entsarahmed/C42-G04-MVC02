83517cd67101ea01859a7cb1f9866488f9fe2f2a
entsarahmed <entesara500@gmail.com>
2024-09-08 15:14:25 +0300

    Layout and _ViewStart

---
+++ Project Structure/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Structure.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
